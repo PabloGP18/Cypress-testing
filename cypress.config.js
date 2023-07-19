77ab8dbb9d26accfda31a903fb0be9e9ed10f0fd
Pablo GP <garciaplaza.pablo@gmail.com>
2023-07-19 10:33:58 +0200

    Updated config with baseurl and env variables

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:8080',
+    env: {
+      navbartText: 'cypress.io',
+      navbartText2: 'Utilities'
+    }
+  }
+})
