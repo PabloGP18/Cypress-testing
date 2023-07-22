3b1830f8025fea9d258e3b5641c44d76b783828b
Pablo GP <garciaplaza.pablo@gmail.com>
2023-07-22 15:44:50 +0200

    Removed init objet

--- cypress/e2e/tutorial-testing/testing-with-POM/firstTestWithPOM.cy.js
+++ cypress/e2e/tutorial-testing/testing-with-POM/firstTestWithPOM.cy.js
@@ -1,25 +1,23 @@
 import ActionsPage from './ActionsPage'
 
-const actionPage = new ActionsPage()
-
 context('Redoing first test with POM', () => {
   beforeEach(() => {
-    actionPage.visit()
+    ActionsPage.visit()
   })
 
   it('has an h1 on the page', () => {
-    actionPage.checkHeaderExist()
+    ActionsPage.checkHeaderExist()
   })
 
   it('renders the correct h1 text', () => {
-    actionPage.checkHeaderText('Actions')
+    ActionsPage.checkHeaderText('Actions')
   })
 
   it('renders a paragraph under the h1', () => {
-    actionPage.checkParagraphUnderHeader(1)
+    ActionsPage.checkParagraphUnderHeader(1)
   })
 
   it('renders a section with the correct elements', () => {
-    actionPage.checkElementInSection(2)
+    ActionsPage.checkElementInSection(2)
   })
 })
