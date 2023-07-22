ee93c243e1d5752d9f9df4f7fba00ac450999835
Pablo GP <garciaplaza.pablo@gmail.com>
2023-07-22 15:33:05 +0200

    Refactoring code not using constructor and this

--- cypress/e2e/tutorial-testing/testing-with-POM/ActionsPage.js
+++ cypress/e2e/tutorial-testing/testing-with-POM/ActionsPage.js
@@ -1,39 +1,35 @@
 class ActionsPage {
-  constructor() {
-    this.header = 'h1'
-    this.container = '.container'
-    this.paragraph = 'p'
-    this.subheader = 'h4'
-  }
+  header = 'h1'
+  container = '.container'
+  paragraph = 'p'
+  subheader = 'h4'
 
   visit() {
     cy.visit('http://example.cypress.io/commands/actions')
-    return this
   }
 
   checkHeaderExist() {
-    cy.get(this.header).should('exist')
-    return this
+    cy.get(ActionsPage.header).should('exist')
   }
 
   checkHeaderText(text) {
-    cy.get(this.header).should('contain.text', text)
-    return this
+    cy.get(ActionsPage.header).should('contain.text', text)
   }
 
   checkParagraphUnderHeader(index) {
-    cy.get(this.container).eq(index).find(this.paragraph).should('exist')
-    return this
+    cy.get(ActionsPage.container)
+      .eq(index)
+      .find(ActionsPage.paragraph)
+      .should('exist')
   }
 
   checkElementInSection(index) {
-    cy.get(this.container)
+    cy.get(ActionsPage.container)
       .eq(index)
       .within(() => {
-        cy.get(this.subheader).should('exist')
-        cy.get(this.paragraph).should('exist')
+        cy.get(ActionsPage.subheader).should('exist')
+        cy.get(ActionsPage.paragraph).should('exist')
       })
-    return this
   }
 }
 
